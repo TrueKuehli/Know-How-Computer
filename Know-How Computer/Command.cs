e921042154f7ba6a4e04559318b1e1413ffeceee
Dennis Office <Dennis Office@ND-2>
2016-11-02 17:50:49 +0100

    added Command framework

---
+++ Know-How Computer/Command.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Know_How_Computer
+{
+    class Command
+    {
+        public int data;
+
+       
+
+
+    }
+
+    class Inc : Command
+    {
+        public Inc(int data)
+        {
+            this.data = data;
+        }
+        //public int Execute()
+        //{
+
+        //}
+    }
+    class Dec : Command
+    {
+        public Dec(int data)
+        {
+            this.data = data;
+        }
+    }
+    class IfZero : Command
+    {
+        public IfZero(int data)
+        {
+            this.data = data;
+        }
+    }
+    class Jump : Command
+    {
+        public Jump(int data)
+        {
+            this.data = data;
+        }
+    }
+    class Stop : Command
+    {
+        public Stop()
+        {  }
+
+    }
+}
--- Know-How Computer/Form1.cs
+++ Know-How Computer/Form1.cs
@@ -50,4 +50,5 @@ namespace Know_How_Computer
 
         }
     }
+
 }
